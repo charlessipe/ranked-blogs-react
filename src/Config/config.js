d340cbf761452e5c23412fcb952721ce8be0f5cd
Charles Sipe <csipe84@gmail.com>
2018-02-19 20:04:02 -0800

    notes not rendering

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyBP2FqLwxN9AtH8oGBoudAjCm5gP4Ak_3I",
+  authDomain: "rankedblogsreact.firebaseapp.com",
+  databaseURL: "https://rankedblogsreact.firebaseio.com",
+  projectId: "rankedblogsreact",
+  storageBucket: "rankedblogsreact.appspot.com",
+  messagingSenderId: "107791531609"
+};
