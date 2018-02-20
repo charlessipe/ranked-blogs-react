f4531b49398046553c3a64fe4cf4be83becd902f
Charles Sipe <csipe84@gmail.com>
2018-02-19 20:29:40 -0800

    add firebase config

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,8 +1,10 @@
 export const DB_CONFIG = {
-  apiKey: "AIzaSyBP2FqLwxN9AtH8oGBoudAjCm5gP4Ak_3I",
-  authDomain: "rankedblogsreact.firebaseapp.com",
-  databaseURL: "https://rankedblogsreact.firebaseio.com",
-  projectId: "rankedblogsreact",
-  storageBucket: "rankedblogsreact.appspot.com",
-  messagingSenderId: "107791531609"
+  apiKey: "AIzaSyA7jb8I-opd6NvHCIG3nCo0sifRv7fzd9o",
+  authDomain: "rankedblogs2.firebaseapp.com",
+  databaseURL: "https://rankedblogs2.firebaseio.com",
+  projectId: "rankedblogs2",
+  storageBucket: "rankedblogs2.appspot.com",
+  messagingSenderId: "272852622644"
 };
+
+    
